8a56ec8441d1c06981929cd1249b246379c9f8a3
Nikolaj Gronlund Nielsen <nikolaj.groenlund@yahoo.com>
2024-04-06 22:40:02 +0200

    added tmux. navigation now works in tmux and nvim. tmux moved to config

--- bin/install/link-dotfiles.sh
+++ bin/install/link-dotfiles.sh
@@ -3,7 +3,9 @@
 DOTFILES_HOME=/home/nikolaj/ngr/engineer/repos/dotfiles
 DOTFILES_CONFIG_HOME=$DOTFILES_HOME/.config
 DOTFILES_BOOKMARKS_HOME=$DOTFILES_CONFIG_HOME/chromium/Default
+DOTFILES_TMUX_HOME=$DOTFILES_CONFIG_HOME/tmux
 CONFIG_HOME=~/.config
+CONFIG_TMUX_HOME=$CONFIG_HOME/tmux
 
 source ./utils.sh
 
@@ -14,3 +16,4 @@ create_file_link $DOTFILES_HOME/.xbindkeysrc ~/.xbindkeysrc
 create_folder_link $DOTFILES_CONFIG_HOME/lf $CONFIG_HOME/lf
 create_folder_link $DOTFILES_CONFIG_HOME/nvim $CONFIG_HOME/nvim
 create_file_link $DOTFILES_HOME/.tmux.conf ~/.tmux.conf
+create_file_link $DOTFILES_TMUX_HOME/.tmux.conf $CONFIG_TMUX_HOME/.tmux.conf
