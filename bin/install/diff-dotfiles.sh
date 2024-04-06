e75ea33394922c2a883bd4aae3c65b648bbeb2da
Nikolaj Gronlund Nielsen <nikolaj.groenlund@yahoo.com>
2024-04-06 23:22:56 +0200

    tmux.conf file placed in root of dotfiles

--- bin/install/diff-dotfiles.sh
+++ bin/install/diff-dotfiles.sh
@@ -3,10 +3,8 @@
 DOTFILES_HOME=/home/nikolaj/ngr/engineer/repos/dotfiles
 DOTFILES_CONFIG_HOME=$DOTFILES_HOME/.config
 DOTFILES_BOOKMARKS_HOME=$DOTFILES_CONFIG_HOME/chromium/Default
-DOTFILES_TMUX_HOME=$DOTFILES_CONFIG_HOME/tmux
 CONFIG_HOME=~/.config
 CONFIG_BOOKMARKS_HOME=$CONFIG_HOME/chromium/Default
-CONFIG_TMUX_HOME=$CONFIG_HOME/tmux
 
 source ./utils.sh
 
@@ -17,4 +15,4 @@ diff_dotfile $DOTFILES_HOME/.xbindkeysrc ~/.xbindkeysrc
 diff_dotfile $DOTFILES_BOOKMARKS_HOME/Bookmarks $CONFIG_BOOKMARKS_HOME/Bookmarks
 diff_folder $DOTFILES_CONFIG_HOME/lf $CONFIG_HOME/lf
 diff_folder $DOTFILES_CONFIG_HOME/nvim $CONFIG_HOME/nvim
-diff_dotfile $DOTFILES_TMUX_HOME/.tmux.conf $CONFIG_TMUX_HOME/.tmux.conf
+diff_dotfile $DOTFILES_HOME/.tmux.conf ~/.tmux.conf
--- bin/install/link-dotfiles.sh
+++ bin/install/link-dotfiles.sh
@@ -3,9 +3,7 @@
 DOTFILES_HOME=/home/nikolaj/ngr/engineer/repos/dotfiles
 DOTFILES_CONFIG_HOME=$DOTFILES_HOME/.config
 DOTFILES_BOOKMARKS_HOME=$DOTFILES_CONFIG_HOME/chromium/Default
-DOTFILES_TMUX_HOME=$DOTFILES_CONFIG_HOME/tmux
 CONFIG_HOME=~/.config
-CONFIG_TMUX_HOME=$CONFIG_HOME/tmux
 
 source ./utils.sh
 
@@ -15,5 +13,4 @@ create_file_link $DOTFILES_HOME/.Xdefaults ~/.Xdefaults
 create_file_link $DOTFILES_HOME/.xbindkeysrc ~/.xbindkeysrc
 create_folder_link $DOTFILES_CONFIG_HOME/lf $CONFIG_HOME/lf
 create_folder_link $DOTFILES_CONFIG_HOME/nvim $CONFIG_HOME/nvim
-create_folder $CONFIG_TMUX_HOME
-create_file_link $DOTFILES_TMUX_HOME/.tmux.conf $CONFIG_TMUX_HOME/.tmux.conf
+create_file_link $DOTFILES_HOME/.tmux.conf ~/.tmux.conf
