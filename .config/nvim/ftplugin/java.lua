ad401024e5ba8bc590b9d4bad111a364f96e9c6f
Nikolaj Gronlund Nielsen <nikolaj.groenlund@yahoo.com>
2024-04-14 14:58:21 +0200

    trying to make lombok work. backup of bookmark

--- .config/nvim/ftplugin/java.lua
+++ .config/nvim/ftplugin/java.lua
@@ -7,6 +7,8 @@ end
 local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
 local workspace_dir = '/home/nikolaj/ngr/engineer/repos/' .. project_name
 
+local lombok_path = "/home/nikolaj/.local/share/nvim/mason/packages/jdtls/lombok.jar"
+
 local config = {
 	cmd = {
     --vim.fn.expand("~/.local/share/nvim/mason/bin/jdtls") },
@@ -16,17 +18,23 @@ local config = {
     '-Declipse.product=org.eclipse.jdt.ls.core.product',
     '-Dlog.protocol=true',
     '-Dlog.level=ALL',
+    '-javaagent:' .. lombok_path,
+    '-Xbootclasspath/a:' .. lombok_path,
     '-Xmx1g',
     '--add-modules=ALL-SYSTEM',
     '--add-opens', 'java.base/java.util=ALL-UNNAMED',
     '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+    --'--jvm-arg=-javaagent:', '/home/nikolaj/.local/share/nvim/mason/packages/jdtls/lombok.jar',
+    --'--jvm-arg=-Xbootclasspath/a:', '/home/nikolaj/.local/share/nvim/mason/packages/jdtls/lombok.jar',
+    --'-javaagent=', '/home/nikolaj/.local/share/nvim/mason/packages/jdtls/lombok.jar',
+    --'-Xbootclasspath/a=', '/home/nikolaj/.local/share/nvim/mason/packages/jdtls/lombok.jar',
     '-jar', '/home/nikolaj/.local/share/nvim/mason/share/jdtls/plugins/org.eclipse.equinox.launcher.jar',
     '-configuration', '/home/nikolaj/.local/share/nvim/mason/packages/jdtls/config_linux',
     '-data', workspace_dir,
   },
 
 	--root_dir = vim.fs.dirname(vim.fs.find({ ".gradlew", ".git", "mvnw" }, { upward = true })[1]),
-  root_dir = require('jdtls.setup').find_root({'.git', 'mvnw', 'gradlew'}),
+  root_dir = require('jdtls.setup').find_root({'.git', 'mvnw', 'gradlew', 'pom.xml', 'build.gradle'}),
 
   settings = {
     java = {}
