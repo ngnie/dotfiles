1d50fbeee214710758f6d03c48e1044d73d36c49
Nikolaj Gronlund Nielsen <nikolaj.groenlund@yahoo.com>
2024-04-14 12:27:02 +0200

    added silent to dapui keymaps

--- .config/nvim/lua/plugins/nvim-dap.lua
+++ .config/nvim/lua/plugins/nvim-dap.lua
@@ -9,9 +9,6 @@ return {
     "ryanoasis/vim-devicons"
   },
   config = function ()
-    --require('dap').setup({})
-    --require('nvim-dap-ui').setup({})
-    --
 
     local status, jdtls = pcall(require, "jdtls")
     if not status then
@@ -31,10 +28,10 @@ return {
       },
     })
 
-    vim.keymap.set("n", "<leader>duio", ":lua require('dapui').open()<cr>", { desc = "Open dapui" })
-    vim.keymap.set("n", "<leader>duic", ":lua require('dapui').close()<cr>", { desc = "Close dapui" })
-    vim.keymap.set("n", "<leader>duit", ":lua require('dapui').toggle()<cr>", { desc = "Toggle dapui" })
-    vim.keymap.set("n", "<leader>de", ":lua require('dapui').eval()<cr>", { desc = "Eval dapui" })
+    vim.keymap.set("n", "<leader>duio", ":lua require('dapui').open()<cr>", { desc = "Open dapui", silent = true  })
+    vim.keymap.set("n", "<leader>duic", ":lua require('dapui').close()<cr>", { desc = "Close dapui", silent = true  })
+    vim.keymap.set("n", "<leader>duit", ":lua require('dapui').toggle()<cr>", { desc = "Toggle dapui", silent = true })
+    vim.keymap.set("n", "<leader>de", ":lua require('dapui').eval()<cr>", { desc = "Eval dapui", silent = true  })
 
   end,
 }
