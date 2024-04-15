2f0412a34bf926a69995eced6ce9aedb9ee3a5e2
Nikolaj Gronlund Nielsen <nikolaj.groenlund@yahoo.com>
2024-04-15 11:18:55 +0200

    added picom opacity

--- bin/install/diff-dotfiles.sh
+++ bin/install/diff-dotfiles.sh
@@ -4,9 +4,11 @@ DOTFILES_HOME=/home/nikolaj/ngr/engineer/repos/dotfiles
 DOTFILES_CONFIG_HOME=$DOTFILES_HOME/.config
 DOTFILES_BOOKMARKS_HOME=$DOTFILES_CONFIG_HOME/chromium/Default
 DOTFILES_ALACRITTY_HOME=$DOTFILES_CONFIG_HOME/alacritty
+DOTFILES_PICOM_HOME=$DOTFILES_CONFIG_HOME/picom
 CONFIG_HOME=~/.config
 CONFIG_BOOKMARKS_HOME=$CONFIG_HOME/chromium/Default
 CONFIG_ALACRITTY_HOME=$CONFIG_HOME/alacritty
+CONFIG_PICOM_HOME=$CONFIG_HOME/picom
 
 source ./utils.sh
 
@@ -19,3 +21,5 @@ diff_folder $DOTFILES_CONFIG_HOME/lf $CONFIG_HOME/lf
 diff_folder $DOTFILES_CONFIG_HOME/nvim $CONFIG_HOME/nvim
 diff_dotfile $DOTFILES_HOME/.tmux.conf ~/.tmux.conf
 diff_dotfile $DOTFILES_ALACRITTY_HOME/alacritty.toml $CONFIG_ALACRITTY_HOME/alacritty.toml
+diff_dotfile $DOTFILES_PICOM_HOME/picom.conf $CONFIG_PICOM_HOME/picom.conf
+
--- bin/install/link-dotfiles.sh
+++ bin/install/link-dotfiles.sh
@@ -4,8 +4,10 @@ DOTFILES_HOME=/home/nikolaj/ngr/engineer/repos/dotfiles
 DOTFILES_CONFIG_HOME=$DOTFILES_HOME/.config
 DOTFILES_BOOKMARKS_HOME=$DOTFILES_CONFIG_HOME/chromium/Default
 DOTFILES_ALACRITTY_HOME=$DOTFILES_CONFIG_HOME/alacritty
+DOTFILES_PICOM_HOME=$DOTFILES_CONFIG_HOME/picom
 CONFIG_HOME=~/.config
 CONFIG_ALACRITTY_HOME=$CONFIG_HOME/alacritty
+CONFIG_PICOM_HOME=$CONFIG_HOME/picom
 
 source ./utils.sh
 
@@ -17,3 +19,4 @@ create_folder_link $DOTFILES_CONFIG_HOME/lf $CONFIG_HOME/lf
 create_folder_link $DOTFILES_CONFIG_HOME/nvim $CONFIG_HOME/nvim
 create_file_link $DOTFILES_HOME/.tmux.conf ~/.tmux.conf
 create_file_link $DOTFILES_ALACRITTY_HOME/alacritty.toml $CONFIG_ALACRITTY_HOME/alacritty.toml
+create_file_link $DOTFILES_PICOM_HOME/picom.conf $CONFIG_PICOM_HOME/picom.conf
