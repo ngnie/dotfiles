f54b0c7d00925c265fb1e37b283bdaa5234cb26e
Nikolaj Grønlund Nielsen <nikolaj.groenlund@yahoo.com>
2024-08-02 10:05:43 +0200

    added tmux and lf to sudo-install-packages.sh. removed LF_HOME and JAVA_HOME from zsh

--- bin/install/sudo-install-packages.sh
+++ bin/install/sudo-install-packages.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-pacman -S git base-devel git libx11 libxft xorg-server xorg-xinit libxinerama zsh chromium ttf-font-awesome neovim alsa-utils pamixer man-db man-pages nvidia nvidia-settings inxi xorg-xrandr xorg-xdpyinfo glxinfo xorg-xlsfonts xbindkeys dnsutils whois go wget bc bat fzf ripgrep scrot feh zathura zathura-pdf-poppler unzip zip less alacritty xsel xclip lazygit python python-pynvim picom
+pacman -S git base-devel git libx11 libxft xorg-server xorg-xinit libxinerama zsh chromium ttf-font-awesome neovim alsa-utils pamixer man-db man-pages nvidia nvidia-settings inxi xorg-xrandr xorg-xdpyinfo glxinfo xorg-xlsfonts xbindkeys dnsutils whois go wget bc bat fzf ripgrep scrot feh zathura zathura-pdf-poppler unzip zip less alacritty xsel xclip lazygit python python-pynvim picom tmux lf
