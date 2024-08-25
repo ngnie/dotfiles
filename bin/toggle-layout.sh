de675fafacc6d5ad4438c5a0e570412f80c0b9c8
Nikolaj Grønlund Nielsen <nikolaj.groenlund@yahoo.com>
2024-08-25 16:54:37 +0200

    added toggle-layout.sh using mod4 u. overriding mod4 with alt tab in dwm

---
+++ bin/toggle-layout.sh
@@ -0,0 +1,9 @@
+#/bin/sh
+
+keyboard_layout=$(setxkbmap -query | grep layout | sed -e 's/ \{1,\}/ /g' | cut -d ' ' -f 2)
+
+if [ $keyboard_layout = "us" ]; then
+  setxkbmap dk
+else
+  setxkbmap us
+fi
