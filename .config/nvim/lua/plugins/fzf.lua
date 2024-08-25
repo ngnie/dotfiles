e5b25dc0365a7d48cbd300412416f26a8b595078
Nikolaj Grønlund Nielsen <nikolaj.groenlund@yahoo.com>
2024-08-25 23:04:17 +0200

    fzf keys changed eg buffer key at space j. caps lock is now esc

--- .config/nvim/lua/plugins/fzf.lua
+++ .config/nvim/lua/plugins/fzf.lua
@@ -10,11 +10,11 @@ return {
   keys = {
 --rg --max-depth=10 --hidden -e Lf
     {"<leader>g", ":GFiles<cr>", silent = true, desc = "Fuzzy find git files"},
-    {"<leader>j", ":Files!<cr>", desc = "Fuzzy find from project root"},
-    {"<leader>k", ":Files! ~<cr>", desc = "Fuzzy find from home"},
-    {"<leader>l", ":Rg3<cr>", desc = "Ripgrep"},
-    {"<leader>L", ":Commands!<cr>", desc = "Fuzzy find command"},
-    {"<leader>;", ":Buffers!<cr>", desc = "Fuzzy find buffers"},
+    {"<leader>j", ":Buffers!<cr>", desc = "Fuzzy find buffers"},
+    {"<leader>k", ":Rg3<cr>", desc = "Ripgrep"},
+    {"<leader>l", ":Files!<cr>", desc = "Fuzzy find from project root"},
+    {"<leader>;", ":Files! ~<cr>", desc = "Fuzzy find from home"},
+    {"<leader>J", ":Commands!<cr>", desc = "Fuzzy find command"},
     {"<leader>/", ":BLines!<cr>", desc = "Fuzzy find in file"},
   },
 }
