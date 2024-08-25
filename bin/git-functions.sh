238209fcc7cc242833c309e231579b85a04c4e8a
Nikolaj Grønlund Nielsen <nikolaj.groenlund@yahoo.com>
2024-08-25 22:31:43 +0200

    gview and gcheckout now working. original from frensh woman.

--- bin/git-functions.sh
+++ bin/git-functions.sh
@@ -2,9 +2,8 @@ gaaa() {
   git log --graph --date-order --date=short --pretty='format:%C(cyan)%h %C(blue)%ar%C(auto)%d %C(yellow)%s%+b %C(black)%ae'
 }
 
-gview() {
+gv() {
   git log \
-    --reverse \
     --color=always \
     --format='format:%C(cyan)%h %C(blue)%ar%C(auto)%d %C(yellow)%s%+b %C(black)%ae' |
     fzf -i -e +s \
@@ -20,4 +19,35 @@ gview() {
       --header "enter: view, C-c: copy hash" \
       --bind "enter:execute:$_viewGitLogLine | less -R" \
       --bind "ctrl-c:execute:$_gitLogLineToHash | xclip -r -selection clipboard"
+}
+
+alias glNoGraph='git log --color=always --format="format:%C(cyan)%h %C(blue)%ar%C(auto)%d %C(yellow)%s%+b %C(black)%ae" "$@"'
+_gitLogLineToHash="echo {} | grep -o '[a-f0-9]\{7\}' | head -1"
+_viewGitLogLine="$_gitLogLineToHash | xargs -I % sh -c 'git show --color=always % | diff-so-fancy'"
+
+gcheckout() {
+  local commit
+  commit=$( glNoGraph |
+    fzf \
+      --no-sort \
+      --reverse \
+      --tiebreak=index \
+      --no-multi \
+      --ansi \
+      --preview="$_viewGitLogLine" ) &&
+  git checkout $(echo "$commit" | sed "s/ .*//")
+}
+
+gview() {
+  glNoGraph |
+  fzf \
+    --no-sort \
+    --reverse \
+    --tiebreak=index \
+    --no-multi \
+    --ansi \
+    --preview="$_viewGitLogLine" \
+    --header "enter to view, ctrl-y to copy hash" \
+    --bind "enter:execute:$_viewGitLogLine   | less -R -c" \
+    --bind "ctrl-y:execute:$_gitLogLineToHash | xclip -r -selection clipboard"
 }
