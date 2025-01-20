276291fbd51564f7bc0156570119ecdcc795ccf1
Nikolaj Grønlund Nielsen <nikolaj.groenlund@yahoo.com>
2025-01-20 10:10:28 +0100

    cleanup in trading folder

--- bin/tmux-trading.sh
+++ bin/tmux-trading.sh
@@ -8,7 +8,7 @@ tmux new-session -d -s $session
 
 # make dotfiles window
 tmux rename-window -t $session:1 'trading'
-tmux send-keys -t $session:1 'cd $TRADING_HOME' C-m
+tmux send-keys -t $session:1 'cd $TRADING_HOME/resources/saxobank' C-m
 tmux send-keys -t $session:1 'nvim account.txt' C-m 
 
 tmux selectw -t $session:1
